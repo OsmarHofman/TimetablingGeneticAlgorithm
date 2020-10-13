8216900c19f32c28a2aa473986af5c79d089c28b
Osmar José Hofman da Silva <osmarjhdas@hotmail.com>
2020-10-13 12:12:59 -0300

    Ajustado para realizar o cruzamento a partir de uma lista e intraturma.

--- src/main/java/br/edu/ifsc/TimetablingGeneticAlgorithm/genetics/Crossover.java
+++ src/main/java/br/edu/ifsc/TimetablingGeneticAlgorithm/genetics/Crossover.java
@@ -16,91 +16,96 @@ public class Crossover {
      * @param crossPercentage porcentagem de cruzamento, ou seja, 10%, 20%, etc.
      * @return nova população de {@link Chromosome}.
      */
-    public static Chromosome[] cross(Chromosome[] chromosomes, int classSize, int crossPercentage){
+    public static Chromosome[] cross(Chromosome[] chromosomes, int classSize, int crossPercentage) {
         Random random = new Random();
         Chromosome[] newGeneration = new Chromosome[chromosomes.length];
         int size = chromosomes[0].getGenes().length;
 
+
+        List<Integer> changeListC1;
+        List<Integer> changeListC2;
+
         for (int i = 0; i < chromosomes.length; i += 2) {
+
             int crossChance = random.nextInt(100);
-            Chromosome p1 = new Chromosome(chromosomes[i].getGenes(), 0);
-            Chromosome p2 = new Chromosome(chromosomes[i + 1].getGenes(), 0);
+            Chromosome c1 = new Chromosome(chromosomes[i].getGenes(), 0);
+            Chromosome c2 = new Chromosome(chromosomes[i + 1].getGenes(), 0);
 
-            //se cruzar
+            //Se cruzar
             if (crossChance < crossPercentage) {
-                int group = random.nextInt(size) / classSize;
-                //TODO refazer para fazer o cruzamento intraturma a partir de um arraylist
 
-                //variável que indica a metade inferior do número de matérias. Isso para garantir que o primeiro
-                //valor aleatório para ponte de corte, será menor que o segundo.
-                int infLimit = group * classSize;
-                int cutPoint1 = random.nextInt(classSize / 2) + infLimit;
-                int cutPoint2 = random.nextInt(classSize / 2) + infLimit + (classSize / 2);
-                Chromosome c1 = new Chromosome(size);
-                Chromosome c2 = new Chromosome(size);
-                for (int j = cutPoint1 + 1; j <= cutPoint2; j++) {
-                    c1.getGenes()[j] = p1.getGenes()[j];
-                    c2.getGenes()[j] = p2.getGenes()[j];
+                //Listas que irão conter os genes que serão passados aos filhos respectivos
+                changeListC1 = new ArrayList<>();
+                changeListC2 = new ArrayList<>();
+
+                //O cutpoint1 é totalmente aleatório, e o cutpoint2 será na mesma turma do cutpoint1
+                int cutPoint1 = random.nextInt(size);
+
+                //Limite inferior, ou seja, a primeira posição da turma
+                int infLimit = Math.floorDiv(cutPoint1, classSize) * 10;
+
+                //Limite superior, ou seja, a última posição da turma
+                int supLimit = infLimit + classSize;
+
+                int cutPoint2 = random.nextInt(classSize) + infLimit + 1;
+
+                if (cutPoint2 < cutPoint1) {
+                    int aux = cutPoint1;
+                    cutPoint1 = cutPoint2;
+                    cutPoint2 = aux;
                 }
 
-                //passa os gênes dos pais para seus dois filhos
-                transfer(c1, cutPoint1, cutPoint2, p1.getGenes(), p2.getGenes(), size);
-                transfer(c2, cutPoint1, cutPoint2, p2.getGenes(), p1.getGenes(), size);
+                //Adiciona somente os genes das turmas que serão cruzadas para os changeLists
+                for (int j = infLimit; j < supLimit; j++) {
+                    if (c2.getGenes()[j] != 0)
+                        changeListC1.add(c2.getGenes()[j]);
+                    if (c1.getGenes()[j] != 0)
+                        changeListC2.add(c1.getGenes()[j]);
+                }
 
+                //Remove das changeLists os genes do meio dos pontos de cortes dos filhos
+                for (int j = cutPoint1; j < cutPoint2; j++) {
+                    changeListC1.remove((Integer) c1.getGenes()[j]);
+                    changeListC2.remove((Integer) c2.getGenes()[j]);
+                }
+
+                //Passa os gênes dos pais para seus dois filhos
+                insertChangeListGenes(c1, cutPoint1, cutPoint2, changeListC1, infLimit, supLimit);
+                insertChangeListGenes(c2, cutPoint1, cutPoint2, changeListC2, infLimit, supLimit);
+            } else {
                 newGeneration[i] = c1;
                 newGeneration[i + 1] = c2;
-            } else {
-                newGeneration[i] = p1;
-                newGeneration[i + 1] = p2;
             }
         }
         return newGeneration;
     }
 
     /**
-     * Faz a transferência dos genes dos pais {@code p1 e p2} para seu filho {@code child}.
+     * Faz a inserção dos genes da {@code changeList} para um {@code child}.
      *
-     * @param child     {@link Chromosome} que representa o cromossomo que está sendo tracado entre si.
-     * @param cutPoint1 valor numérico que representa o primeiro ponto de corte.
-     * @param cutPoint2 valor numérico que representa o segundo ponto de corte.
-     * @param p1        vetor de int que representa os gênes do primeiro pai.
-     * @param p2        vetor de int que representa os gênes do segundo pai.
-     * @param size      quantidade total de gênes presentes em um cromossomo
+     * @param child      {@link Chromosome} que representa o cromossomo que está sendo tracado entre si.
+     * @param cutPoint1  valor numérico que representa o primeiro ponto de corte.
+     * @param cutPoint2  valor numérico que representa o segundo ponto de corte.
+     * @param changeList Vetor que contém os ids das turmas a serem inseridos no {@code child}.
+     * @param infLimit   {@link Integer} que representa o limite inferior da turma a ser mudada (primeira posição da turma).
+     * @param supLimit   {@link Integer} que representa o limite superior da turma a ser mudada (última posição da turma).
      */
-    private static void transfer(Chromosome child, int cutPoint1, int cutPoint2, int[] p1, int[] p2, int size) {
+
+    private static void insertChangeListGenes(Chromosome child, int cutPoint1, int cutPoint2,
+                                              List<Integer> changeList, int infLimit, int supLimit) {
         int parentIterator = cutPoint2;
-        for (int j = 0; j < size; j++) {
-            if (parentIterator == size)
-                parentIterator = 0;
-            if (isNotRepeated(p1, cutPoint1, cutPoint2, p2[parentIterator])) {
-                if (j < cutPoint1 || j >= cutPoint2) {
-                    for (int i = j; i < j + 10; i++) {
-                        child.getGenes()[i] = p2[parentIterator];
-                        parentIterator++;
-                    }
-                    j += 9;
-                }
-            } else {
-                parentIterator += 10;
-                j--;
+        int changeListIndex = 0;
+        for (int j = infLimit; j < supLimit; j++) {
+
+            //Caso chega no final da turma, volta para o começo
+            if (parentIterator == supLimit)
+                parentIterator = infLimit;
+
+            //Coloca do changeList para o child
+            if (j < cutPoint1 || j >= cutPoint2) {
+                child.getGenes()[j] = changeList.get(changeListIndex);
+                changeListIndex++;
             }
         }
-    }
-
-    /**
-     * Verifica se um {@code gene} em específico, é igual a algum do {@code p1}.
-     *
-     * @param p1        gênes do primeiro pai.
-     * @param cutPoint1 valor numérico que representa o primeiro ponto de corte.
-     * @param cutPoint2 valor numérico que representa o segundo ponto de corte.
-     * @param gene      gêne a ser verificado se já está contido no {@code p1}.
-     * @return true caso o {@code gene} já esteja no {@code p1}, e else caso contrário.
-     */
-    private static boolean isNotRepeated(int[] p1, int cutPoint1, int cutPoint2, int gene) {
-        for (int i = cutPoint1; i < cutPoint2; i++) {
-            if (p1[i] == gene && p1[i] != 0)
-                return false;
-        }
-        return true;
     }
 }
